01325b4d9a4fe022e7b9894b41580ffe54d16058
w0rd-glitch <panaite.cristian12@gmail.com>
2022-01-16 11:25:56 +0200

    putin responsive

--- src/Components/Navbar/navbar.tsx
+++ src/Components/Navbar/navbar.tsx
@@ -4,61 +4,67 @@ import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className="navbar">
-      <li><Link to="/">
-        <img
-          src={require("../../assets/Images/logo.png").default}
-          alt="logo"
-          className="logo"
-        /></Link>
+      <li className="displayed">
+        <Link to="/">
+          <img
+            src={require("../../assets/Images/logo.png").default}
+            alt="logo"
+            className="logo"
+          />
+        </Link>
       </li>
-      <li>
+      <li className="not-displayed">
         <NavLink end to="/">
           HOME
         </NavLink>
       </li>
-      <li>
+      <li className="not-displayed">
         <NavLink end to="/blog">
           BLOG
         </NavLink>
       </li>
-      <li>
+      <li className="not-displayed">
         <NavLink end to="/awards">
           AWARDS
         </NavLink>
       </li>
-      <li>
+      <li className="not-displayed">
         <NavLink end to="/alumni">
           ALUMNI
         </NavLink>
       </li>
-      <li>
+      <li className="not-displayed">
         <NavLink end to="/login">
           LOGIN
         </NavLink>
       </li>
-      <li>
+      <li className="not-displayed">
         <NavLink end to="/register">
           REGISTER
         </NavLink>
       </li>
-      <li>
+      <li className="not-displayed">
         <a href="https://www.youtube.com/channel/UCoUzT42Ce6f_11ICDef3svA">
           <FontAwesomeIcon icon={faYoutube} />
         </a>
       </li>
-      <li>
+      <li className="not-displayed">
         <a href="https://ro-ro.facebook.com/ThoborCNCH/">
           <FontAwesomeIcon icon={faFacebook} />
         </a>
       </li>
-      <li>
+      <li className="not-displayed">
         <a href="https://www.instagram.com/thoborcnch/">
           <FontAwesomeIcon icon={faInstagram} />
         </a>
+      </li>
+      <li className="icon">
+        <FontAwesomeIcon icon={faBars} />
       </li>
     </div>
   );
