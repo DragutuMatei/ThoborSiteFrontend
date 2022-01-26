c55c2e5718c49478922dd16cfa7392498b8838d1
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2022-01-26 14:37:59 +0200

    daa

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
-import Home from './pages/home/Home';
-import Blog from './pages/blog/Blog';
-import Awards from './pages/awards/Awards';
-import Login from './pages/login/Login';
-import Register from './pages/register/Register';
-import Navbar from './components/navbar/Navbar';
-import AlumniList from "./pages/alumni/AlumniList";
-import Contact from "./pages/contact/Contact";
+import Navbar from "./Components/Navbar/navbar";
+import AlumniList from "./Pages/alumni/AlumniList";
+import Awards from "./Pages/Awards/awards";
+import Blog from "./Pages/Blog/blog";
+import Contact from "./Pages/contact/Contact";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/login";
+import Register from "./Pages/Register/register";
 
 function App() {
   return (
