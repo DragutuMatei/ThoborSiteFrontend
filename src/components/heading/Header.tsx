c02feab84604ea69fe3860d1e16be6b69c6deb98
w0rd-glitch <panaite.cristian12@gmail.com>
2022-03-12 10:00:56 +0200

    nush

--- src/components/heading/Header.tsx
+++ src/components/heading/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="heading">
-      <h1>We are THOBOR</h1>
+      <h1>THOBOR</h1>
       <h3>A team of eleves and elevs from Tecuci</h3>
     </div>
   );
