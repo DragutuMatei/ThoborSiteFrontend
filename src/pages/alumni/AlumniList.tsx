bd809efd16ae08b3d92b1704a574db0297b456f0
w0rd-glitch <panaite.cristian12@gmail.com>
2022-03-14 14:01:50 +0200

    alumni page

--- src/pages/alumni/AlumniList.tsx
+++ src/pages/alumni/AlumniList.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Member from "../../components/member/Member"
 import "./alumniList.scss";
 
 
@@ -10,39 +9,105 @@ function AlumniList() {
         <h2>Alumni</h2>
         <hr></hr>
       </div>
-      <div className="alumni-list">
-        <ul>
-          <Member
-            nume="Chiscop Robert Adrian"
-            imagPath={"/Images/Robica.JPG"}
-            departamente={["Team Leader", "Mecanica & Proiectare 3D"]}
-          />
-          <Member
-            nume="Oprea Marcello Octavian"
-            imagPath={"/Images/Marcelo.jpg"}
-            departamente={["Marketing & PR"]}
-          />
-          <Member
-            nume="Dragutu Matei"
-            imagPath={"/Images/matei.jpg"}
-            departamente={["Programming", "Marketing & PR"]}
-          />
-          <Member
-            nume="Dragutu Matei"
-            imagPath={"/Images/matei.jpg"}
-            departamente={["Programming", "Marketing & PR"]}
-          />
-          <Member
-            nume="Dragutu Matei"
-            imagPath={"/Images/matei.jpg"}
-            departamente={["Programming", "Marketing & PR"]}
-          />
-          <Member
-            nume="Uratul Matei"
-            imagPath={"/Images/matei.jpg"}
-            departamente={["Programming", "Marketing & PR"]}
-          />
-        </ul>
+      <div className="alumni-list1">
+        <div className="orizontal">
+          <div className="orizontal-item">
+            <img
+              src={require("../../assets/Images/Baducu.jpg").default}
+              alt="claudiu"
+            />
+            <div className="item-text">
+              <h2>Baducu</h2>
+              <h3>A ajuns plm undeva</h3>
+              <p>fost rau""Bine a fost cand a fost rau"</p>
+            </div>
+          </div>
+          <div className="orizontal-item">
+            <img
+              src={require("../../assets/Images/Baducu.jpg").default}
+              alt="claudiu"
+            />
+            <div className="item-text">
+              <h2>Baducu</h2>
+              <h3>A ajuns plm undeva</h3>
+              <p>fost rau""Bine a fost cand a fost rau"</p>
+            </div>
+          </div>
+        </div>
+        <div className="vertical">
+          <div className="vertical-item">
+            <img
+              src={require("../../assets/Images/Baducu.jpg").default}
+              alt="claudiu"
+            />
+            <div className="item-text">
+              <h2>Baducu</h2>
+              <h3>A ajuns plm undeva</h3>
+              <p>fost rau""Bine a fost cand a fost rau"</p>
+            </div>
+          </div>
+          <div className="vertical-item">
+            <img
+              src={require("../../assets/Images/Baducu.jpg").default}
+              alt="claudiu"
+            />
+            <div className="item-text">
+              <h2>Baducu</h2>
+              <h3>A ajuns plm undeva</h3>
+              <p>fost rau""Bine a fost cand a fost rau"</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="alumni-list2">
+        <div className="orizontal">
+          <div className="orizontal-item">
+            <img
+              src={require("../../assets/Images/Baducu.jpg").default}
+              alt="claudiu"
+            />
+            <div className="item-text">
+              <h2>Baducu</h2>
+              <h3>A ajuns plm undeva</h3>
+              <p>fost rau""Bine a fost cand a fost rau"</p>
+            </div>
+          </div>
+          <div className="orizontal-item">
+            <img
+              src={require("../../assets/Images/Baducu.jpg").default}
+              alt="claudiu"
+            />
+            <div className="item-text">
+              <h2>Baducu</h2>
+              <h3>A ajuns plm undeva</h3>
+              <p>fost rau""Bine a fost cand a fost rau"</p>
+            </div>
+          </div>
+        </div>
+        <div className="vertical">
+          <div className="vertical-item">
+            <img
+              src={require("../../assets/Images/Baducu.jpg").default}
+              alt="claudiu"
+            />
+            <div className="item-text">
+              <h2>Baducu</h2>
+              <h3>A ajuns plm undeva</h3>
+              <p>fost rau""Bine a fost cand a fost rau"</p>
+            </div>
+          </div>
+          <div className="vertical-item">
+            <img
+              src={require("../../assets/Images/Baducu.jpg").default}
+              alt="claudiu"
+            />
+            <div className="item-text">
+              <h2>Baducu</h2>
+              <h3>A ajuns plm undeva</h3>
+              <p>fost rau""Bine a fost cand a fost rau"</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
