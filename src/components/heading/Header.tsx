9bb20bd6d0d8b45e48f7c243c599884934839d55
w0rd-glitch <panaite.cristian12@gmail.com>
2022-03-30 12:25:49 +0300

    yay

--- src/components/heading/Header.tsx
+++ src/components/heading/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="heading">
       <h1>NOI SUNTEM THOBOR</h1>
-      <h3>O echipa de adolescenti pasionati ce iubesc robotica</h3>
+      <h3>Echipa de robotică, formată din 15 liceeni ai Colegiului Național “Calistrat Hogaș” Tecuci, alături de mentori, îndrăznim să visăm. Plecată dintr-o zonă fără tradiții în domeniu, echipa și-a propus să aducă în comunitate o idee nouă, care să-i inspire pe toți colegii, care să deschidă noi direcții de evoluție pentru copii.</h3>
     </div>
   );
 }
